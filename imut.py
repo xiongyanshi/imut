566366eb85c82c74cc44022dfb8960ce1f0519b9
ysh8 <1240172230@qq.com>
2021-07-19 22:37:45 +0800

    commit

--- imut.py
+++ imut.py
@@ -1,8 +1,20 @@
+import os
+import sys
 import pysam
 import argparse
 
 def main():
-    pass
+    bam_file = sys.argv[1]
+    chrm = 'chr17'
+    pos = 37872045
+
+    samfile = pysam.AlignmentFile(bam_file, 'rb')
+    for ppc in samfile.pileup(chrm, pos-1, pos, truncate=True):
+        print(ppc.get_mapping_qualities())
+        print(ppc.get_num_aligned())
+        print(ppc.get_query_names())
+        print(ppc.get_query_positions())
+        print(ppc.get_query_sequences())
 
 
 if __name__ == '__main__':
