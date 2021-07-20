a2bab8f253244dfd9d4e89922bf0daf5f895716e
xiongyanshi <1240172230@qq.com>
2021-07-20 17:13:53 +0800

    add argparse

--- imut.py
+++ imut.py
@@ -4,6 +4,21 @@ import pysam
 import argparse
 
 def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('func', choices=['snv','indel'], nargs=1)
+    parser.add_argument('bam', nargs=1)
+    parser.add_argument('ref', nargs=1)
+    parser.add_argument('loc', nargs=1)
+    parser.add_argument('--output', nargs='?')
+
+    args = vars(parser.parse_args())
+
+    for k,v in args.items():
+        print(k, v)
+
+    '''
     bam_file = sys.argv[1]
     chrm = 'chr17'
     pos = 37872045
@@ -15,7 +30,7 @@ def main():
         print(ppc.get_query_names())
         print(ppc.get_query_positions())
         print(ppc.get_query_sequences())
-
+    '''
 
 if __name__ == '__main__':
     main()
