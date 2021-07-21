cb9d33f9fbe1fd7eb11c53ef98399a92a701a677
xiongyanshi <1240172230@qq.com>
2021-07-21 18:09:05 +0800

    add

--- imut.py
+++ imut.py
@@ -11,7 +11,8 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('func', choices=['snv','indel'], nargs=1, help='"snv" or "indel"')
+    parser.add_argument('func', choices=['snv','indel'], nargs=1,
+                          help='"snv" or "indel"')
     parser.add_argument('bam', nargs=1, help='bam file')
     parser.add_argument('ref', nargs=1, help='reference fasta, like: hg19.fa')
     parser.add_argument('loc', nargs=1, help='like: chr1:10000')
@@ -23,16 +24,16 @@ def main():
         print(k, v)
 
     bam_file = args['bam'][0]
+    ref = args['ref'][0]
     chrm, pos = args['loc'][0].split(':')
     pos = int(pos)
 
-    bam = pysam.AlignmentFile(bam_file, 'rb')
-    for ppc in bam.pileup(chrm, pos-2, pos, truncate=True):
-        print('ppc.get_mapping_qualities()', ppc.get_mapping_qualities())
-        print('ppc.get_num_aligned()',       ppc.get_num_aligned())
-        print('ppc.get_query_names()',       ppc.get_query_names())
-        print('ppc.get_query_positions()',   ppc.get_query_positions())
-        print('ppc.get_query_sequences()',   ppc.get_query_sequences())
+    bam = pysam.AlignmentFile(bam_file, 'rb', reference_filename=ref)
+    for ppc in bam.pileup(chrm, pos-1, pos, truncate=True):
+        for base, baseq, basep, mapq, read in zip(ppc.get_query_sequences(mark_matches=True),
+                ppc.get_query_qualities(), ppc.get_query_positions(),
+                ppc.get_mapping_qualities(), ppc.pileups):
+            print(base, baseq, basep, mapq, read)
 
 
 if __name__ == '__main__':
